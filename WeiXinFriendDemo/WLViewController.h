b89341e44cb3de197e1a2c8fc3d32e52a44a381e
erichkko <erichkko@163.com>
2016-11-11 19:34:17 +0800

    微信朋友圈的Demo
    微信朋友圈的简单实现

---
+++ WeiXinFriendDemo/WLViewController.h
@@ -0,0 +1,13 @@
+//
+//  WLViewController.h
+//  WeiXinFriendDemo
+//
+//  Created by WangLong on 16/11/11.
+//  Copyright © 2016年 WangLong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLViewController : UIViewController
+
+@end
