ae04d47020be8545f3e093d9f52c7235c635e832
erichkko <erichkko@163.com>
2016-11-11 19:23:28 +0800

    Initial Commit

---
+++ WeiXinFriendDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiXinFriendDemo
+//
+//  Created by WangLong on 16/11/11.
+//  Copyright © 2016年 WangLong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeiXinFriendDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeiXinFriendDemo
+//
+//  Created by WangLong on 16/11/11.
+//  Copyright © 2016年 WangLong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
